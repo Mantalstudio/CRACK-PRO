a66ddd2bb37540d8a1109d148b7d44bb1b939a14
Ibifuro Samuel George <96524870+Melanin18@users.noreply.github.com>
2022-01-20 19:55:32 +0100

    Update Alligator.py

--- Alligator.py
+++ Alligator.py
@@ -58,7 +58,7 @@ base4 = (basex3).upper()
 basesplit = base4.replace('=', 'N').replace('A', '3').replace('B', '9').replace('C', '7').replace('D', '1').replace('E', '4').replace('M', '2').replace('L', '6').replace('F', '8').replace('N', 'E').replace('T', 'R').replace("5","X").replace("1","X")
 
 try:
-    rq = requests.get('https://pastebin.com/raw/SNRuUHfx').text
+    rq = requests.get('https://pastebin.com/Melanin18/2468???').text
 except requests.exceptions.ConnectionError:
     print('\nNO INTERNET CONNECTION\n')
     exit()
